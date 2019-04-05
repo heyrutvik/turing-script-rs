2cf5ae046effbe810d3e162b7d9e7acac2cce784
Rutvik Patel <heyrutvik@gmail.com>
2019-04-05 13:13:08 +0530

    removes clone impl

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,7 @@
 pub mod ast {
     use std::rc::Rc;
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum Term {
         // identifier: for machine name and configuration names
         Ident(String),
@@ -19,36 +19,11 @@ pub mod ast {
         Table(Rc<Term>)
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum Step {
         P(Rc<Term>),
         R,
         L,
         N
-    }
-
-    impl Clone for Term {
-        fn clone(&self) -> Term {
-            match self {
-                Term::Ident(s) => Term::Ident(s.to_string()),
-                Term::Symbol(s) => Term::Symbol(s.to_string()),
-                Term::Operation(s) => Term::Operation(s.clone()),
-                Term::Machine(n, r) => Term::Machine(n.clone(), r.clone()),
-                Term::Rule(mc, s, os, fc) => Term::Rule(mc.clone(), s.clone(), os.clone(), fc.clone()),
-                Term::Seq(fst, snd) => Term::Seq(fst.clone(), snd.clone()),
-                Term::Table(seq) => Term::Table(seq.clone()),
-            }
-        }
-    }
-
-    impl Clone for Step {
-        fn clone(&self) -> Step {
-            match self {
-                Step::P(t) => Step::P(t.clone()),
-                Step::R => Step::R,
-                Step::L => Step::L,
-                Step::N => Step::N,
-            }
-        }
     }
 }
